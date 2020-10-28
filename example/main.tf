6e49a7cf161e58a3e6e947f5336853aaa5c660d9
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-28 16:40:09 +0100

    fix issues with the keys; fix the example

--- example/main.tf
+++ example/main.tf
@@ -10,10 +10,25 @@ variable "aws_region" {
   default     = "eu-central-1"
   description = "AWS region"
 }
+
+locals {
+  tmp_path  = "${path.module}/.terraform/tmp"
+  public_key_path = "${local.tmp_path}/key_rsa"
+  ansible_command_engine = "ansible-playbook -i ${module.instance.public_ip}, --user ubuntu --private-key ${local.public_key_path} playbook.yml"
+}
+
 resource "tls_private_key" "default" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+
+resource "local_file" "private_key" {
+  content = tls_private_key.default.private_key_pem
+  filename = local.public_key_path
+  file_permission = "0600"
+}
+
 
 provider "aws" {
   region     = var.aws_region
@@ -25,5 +40,14 @@ provider "aws" {
 module "instance" {
   description = "test instance"
   source      = "../"
-  public_key = "${tls_private_key.default.public_key_openssh}"
+  public_key = tls_private_key.default.public_key_openssh
+}
+
+
+output "run_ansible_command" {
+  value = local.ansible_command_engine
+}
+
+output "ssh_command" {
+  value = "ssh -i ${local.public_key_path} ubuntu@${module.instance.public_ip}"
 }
